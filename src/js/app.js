d59bba2092c5a07eb14943771ac0c5a1c2ee6280
A. Maslarov <nsite2014@gmail.com>
2023-03-10 16:52:12 +0200

    Added hot emogi

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,13 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  let price = document.getElementsByClassName("price")
+  for (let i = 0; i < price.length; i++) {
+    if(price[i].classList.contains("hot")) {
+      let hotPrice = price[i].textContent;
+      price[i].textContent = hotPrice + " 🔥"
+      console.log()
+    }
+  }
 });
